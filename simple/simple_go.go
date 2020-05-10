75e963e9e5c488a024213a89a74655291cd6aa1e
dgreat91 <dgreat91@gmail.com>
2020-05-10 16:55:11 +0800

    Tested calling package Reverse from main package simple

--- simple/simple_go.go
+++ simple/simple_go.go
@@ -1,9 +1,12 @@
-package main
+package simple
 
 import (
 	"fmt"
+
+	"github.com/dgreat91/simple_go/stringutil"
 )
 
 func main() {
 	fmt.Printf("hello, world\n")
+	fmt.Println(stringutil.Reverse("!oG ,olleH"))
 }
