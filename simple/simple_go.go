90a3f58138c67d75beeff0ab59e83eb10d8cb132
dgreat91 <dgreat91@gmail.com>
2020-05-10 17:31:30 +0800

    Tested importing the octopus package and accessing the class and it's attributes and functions from the pain package

--- simple/simple_go.go
+++ simple/simple_go.go
@@ -1,13 +1,20 @@
-package simple
+package main
 
 import (
 	"fmt"
 
+	"github.com/dgreat91/simple_go/octopus"
 	"github.com/dgreat91/simple_go/stringutil"
 )
 
 func main() {
 	fmt.Printf("hello, world\n")
-	fmt.Println(stringutil.Reverse("!oG ,olleH"))
+	fmt.Println(stringutil.Reverse("!oG ,ereht olleH") + "\n")
 
+	oct := octopus.Octopus{
+		Name:  "Jesse",
+		Color: "orange",
+	}
+
+	fmt.Println(oct.String())
 }
